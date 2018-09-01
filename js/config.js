7e0e21ff0585ff870d56214fcd7dcfe699f93ab6
behzad-vahedi <vahedi.behzad@gmail.com>
2018-08-31 23:49:25 -0700

    New Website

---
+++ js/config.js
@@ -0,0 +1,25 @@
+/******************************************************************************************************************/
+/* (c) Behzad Vahedi                                                                                        */
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["GIScientist", "Developer", "Researcher"];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Front page effect - can be 'none', 'particles', 'particle-fountain', 'video'  */
+var config_frontPageEffect = 'particles';
+
+/* If config_frontPageEffect is set to video, here set the video URL, type and opacity */
+var config_videoUrl = 'video/background.mp4';
+var config_videoType = 'video/mp4';
+var config_videoOpacity = 0.5;
+
+/* Google map parameters */
+var config_mapLat = 34.415977; // latitude
+var config_mapLng = -119.8458988; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'UCSB Department of Geography' // marker title
